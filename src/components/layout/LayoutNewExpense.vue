b60ea1143125d17b5c9a7a2524b6558e99312682
Kaue Depresbiteris <kaue.sobral19@gmail.com>
2023-02-26 22:48:37 -0300

    wip

--- src/components/layout/LayoutNewExpense.vue
+++ src/components/layout/LayoutNewExpense.vue
@@ -24,6 +24,17 @@
                   <label for="">Valor (R$):</label>
                   <input required type="text" v-model="form.value" class="form-control">
                 </div>
+                <div class="form-group col-12 d-flex align-items-center flex-column">
+                  <input ref="input" class="d-none" accept="image/*" type="file" @change="handleFile">
+                  <button type="button" @click="openFileDialog()" class="w-50 my-2 btn btn-outline-secondary">Adicionar
+                    comprovante</button>
+                  <div class="mt-2" v-if="form.receipt">
+                    {{ form.receipt.name }}
+                    <button type="button" @click="form.receipt = ''" class="btn badge badge-light">
+                      <i class="fa fa-trash text-danger"></i>
+                    </button>
+                  </div>
+                </div>
               </div>
             </div>
             <div class="modal-footer">
@@ -39,7 +50,7 @@
 </template>
 <script>
 import { collection, addDoc } from 'firebase/firestore'
-
+import { getStorage, ref, uploadBytes } from 'firebase/storage'
 export default {
   name: 'layoutNewExpense',
 
@@ -48,23 +59,61 @@ export default {
       showModal: false,
       form: {
         description: '',
-        value: ''
+        value: '',
+        receipt: ''
+      }
+    }
+  },
+  computed: {
+    fileName () {
+      const { receipt } = this.form
+      if (receipt) {
+        const split = receipt.name.split('.')
+        return `${split[0]}-${new Date().getTime()}.${split[1]}`
+      } else {
+        return ''
       }
     }
   },
   methods: {
+    handleFile ({ target }) {
+      this.form.receipt = target.files[0]
+    },
+    openFileDialog () {
+      this.$refs.input.value = null
+      this.$refs.input.click()
+    },
     closeModal () {
       this.showModal = false
     },
     async submit () {
       try {
-        const docRef = await addDoc(collection(this.$firebase, 'expense'), {
-        })
+        let uploadTask
+        const id = window.uid
+        const payload = {
+          id,
+          ...this.form,
+          createdAt: new Date()
+        }
+        const docRef = await addDoc(collection(this.$firebase, 'expense'), payload)
+        console.log(this.form.receipt.name)
+        // const docImage = await addDoc(collection(this.$firebase, 'images'), {
+        //   name: this.form.receipt.name
+        // })
+        // console.log(docImage)
+        if (this.form.receipt) {
+          const storage = getStorage()
+          const mountainsRef = ref(storage, this.form.receipt.name)
+          uploadTask = await uploadBytes(mountainsRef, this.form.receipt).then((snapshot) => {
+            console.log('Uploaded a blob or file!', snapshot)
+          })
+          console.log(uploadTask)
+        }
+
         console.log('Document written with ID: ', docRef.id)
       } catch (e) {
         console.error('Error adding document: ', e)
       }
-      console.log('oi')
     }
   }
 }
